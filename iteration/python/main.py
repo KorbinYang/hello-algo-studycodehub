9258e50b0de1288bf817df8dd3dd546f7ade2151
Korbin <korbinyang@qq.com>
2025-03-11 17:14:22 +0800

    feat: iteration -> while 循环

--- iteration/python/main.py
+++ iteration/python/main.py
@@ -9,3 +9,38 @@ def for_loop(n: int) -> int:
 n = 5
 sum = for_loop(n)
 print(f"1到{n}的和为：{sum}")
+
+
+# while 循环实现1到n求和
+def while_loop(n: int) -> int:
+    # while 循环
+    res = 0
+    i = 1  # 初始化条件变量
+    # 循环求和1,2,...,n-1,n
+    while i <= n:
+        res += i
+        i += 1
+    return res
+
+
+m = 5
+sum1 = while_loop(m)
+print(f"1到{m}的和为：{sum1}")
+
+
+# while 循环实现1到n求和, i 每轮进行两次更新
+def while_loop_ii(n: int) -> int:
+    res = 0
+    i = 1
+    # 循环求和1,4,10,...
+    while i <= n:
+        res += i
+        # 更新变量条件
+        i += 1
+        i *= 2
+    return res
+
+
+x = 30
+sum2 = while_loop_ii(x)
+print(f"1到{x}的和为：{sum2}")
--- iteration/ts/main.ts
+++ iteration/ts/main.ts
@@ -1,5 +1,5 @@
 // 1到n求和
-function for_loop(n: number): number {
+export function for_loop(n: number): number {
     let res = 0
 
     for (let i = 1; i <= n; i++) {
@@ -13,4 +13,37 @@ const n = 5
 const sum = for_loop(n)
 console.log(`1到${n}的和为：${sum}`)
 
-export default for_loop
+// while 循环实现1到n求和
+export function while_loop(n: number): number {
+    let res = 0
+    let i = 1
+
+    while (i <= n) {
+        res += i
+        i += 1
+    }
+
+    return res
+}
+
+const m = 5
+const sum1 = while_loop(n)
+console.log(`1到${m}的和为：${sum1}`)
+
+// while 循环实现1到n求和, i 每轮进行两次更新
+export function while_loop_ii(n: number): number {
+    let res = 0
+    let i = 1
+
+    while (i <= n) {
+        res += i
+        i += 1
+        i *= 2
+    }
+
+    return res
+}
+
+const x = 30
+const sum2 = while_loop_ii(x)
+console.log(`1到${x}的和为：${sum2}`)
