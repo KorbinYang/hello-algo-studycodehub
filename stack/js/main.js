d629dc5771d72d7a693870bbf9c7fa5518e9a568
Korbin <korbinyang@qq.com>
2025-04-22 18:35:40 +0800

    feat: 基于链表实现队列

--- stack/js/main.js
+++ stack/js/main.js
@@ -158,3 +158,91 @@ console.log('myArrayStack栈顶元素访问:', myArrayStack.to_list(), myArrayPe
 // 出栈
 const num1 = myArrayStack.pop()
 console.log('myArrayStack出栈:', myArrayStack.to_list(), num1)
+
+// 基于链表实现的队列
+class LinkedListQueue {
+    constructor() {
+        this._front = null
+        this._rear = null
+        this._size = 0
+    }
+
+    size() {
+        return this._size
+    }
+
+    is_empty() {
+        return this.size() === 0
+    }
+
+    push(num) {
+        const node = new ListNode(num)
+
+        if (this.is_empty()) {
+            this._front = node
+            this._rear = node
+        } else {
+            this._rear.next = node
+            this._rear = node
+        }
+
+        this._size += 1
+    }
+
+    pop() {
+        if (this.is_empty()) {
+            throw new Error('队列为空')
+        }
+
+        const num = this._front.val
+        this._front = this._front.next
+        this._size -= 1
+
+        return num
+    }
+
+    peek() {
+        if (this.is_empty()) {
+            throw new Error('队列为空')
+        }
+
+        return this._front.val
+    }
+
+    to_list() {
+        const listArray = []
+        let temp = this._front
+
+        while (temp) {
+            listArray.push(temp.val)
+            temp = temp.next
+        }
+
+        return listArray
+    }
+}
+
+//  实例化队列
+const myLinkedListQueue = new LinkedListQueue()
+console.log('myLinkedListQueue:', myLinkedListQueue.size())
+console.log('myLinkedListQueue:', myLinkedListQueue.is_empty())
+
+//  入队
+myLinkedListQueue.push(1)
+myLinkedListQueue.push(3)
+myLinkedListQueue.push(2)
+myLinkedListQueue.push(5)
+myLinkedListQueue.push(4)
+console.log('myLinkedListQueue 入队:', myLinkedListQueue.to_list())
+
+//  出队
+const frontNum = myLinkedListQueue.pop()
+console.log('myLinkedListQueue 出队:', myLinkedListQueue.to_list(), frontNum)
+
+//  访问队首元素
+const peekNum = myLinkedListQueue.peek()
+console.log(
+    'myLinkedListQueue 访问队首元素:',
+    myLinkedListQueue.to_list(),
+    peekNum
+)
--- stack/python/main.py
+++ stack/python/main.py
@@ -191,3 +191,80 @@ size: int = len(que)
 
 # 判断队列是否为空
 is_empty: bool = len(que) == 0
+
+
+# 基于链表实现的队列
+class LinkedListQueue:
+    """基于链表实现的队列"""
+
+    def __init__(self):
+        """构造方法"""
+        self._front: ListNode | None = None  # 头节点 front
+        self._rear: ListNode | None = None  # 尾节点 rear
+        self._size: int = 0
+
+    def size(self) -> int:
+        """获取队列的长度"""
+        return self._size
+
+    def is_empty(self) -> bool:
+        """判断队列是否为空"""
+        return self.size() == 0
+
+    def push(self, num: int):
+        """入队"""
+        # 在尾节点后添加 num
+        node = ListNode(num)
+        # 如果队列为空，则令头、尾节点都指向该节点
+        if self.is_empty():
+            self._front = node
+            self._rear = node
+        else:
+            self._rear.next = node
+            self._rear = node
+        self._size += 1
+
+    def pop(self) -> int:
+        """出队"""
+        num = self.peek()
+        # 删除头节点
+        self._front = self._front.next
+        self._size -= 1
+        return num
+
+    def peek(self) -> int:
+        """访问队首元素"""
+        if self.is_empty():
+            raise IndexError("队列为空")
+        return self._front.val
+
+    def to_list(self) -> list[int]:
+        """转化为列表用于打印"""
+        queue = []
+        temp = self._front
+        while temp:
+            queue.append(temp.val)
+            temp = temp.next
+        return queue
+
+
+# 实例化队列
+myLinkedListQueue = LinkedListQueue()
+print("myLinkedListQueue:", myLinkedListQueue.size())
+print("myLinkedListQueue:", myLinkedListQueue.is_empty())
+
+# 入队
+myLinkedListQueue.push(1)
+myLinkedListQueue.push(3)
+myLinkedListQueue.push(2)
+myLinkedListQueue.push(5)
+myLinkedListQueue.push(4)
+print("myLinkedListQueue 入队:", myLinkedListQueue.to_list())
+
+# 出队
+frontNum = myLinkedListQueue.pop()
+print("myLinkedListQueue 出队:", myLinkedListQueue.to_list(), frontNum)
+
+# 访问队首元素
+peekNum = myLinkedListQueue.peek()
+print("myLinkedListQueue 访问队首元素:", myLinkedListQueue.to_list(), peekNum)
