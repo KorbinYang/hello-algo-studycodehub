cdab009e574d1329fe1789311d2a486d88c611a9
Korbin <korbinyang@qq.com>
2025-05-30 11:29:51 +0800

    feat: 二叉树初始化及插入删除操作

---
+++ binary_tree/python/main.py
@@ -0,0 +1,32 @@
+class TreeNode:
+    """二叉树结点类"""
+
+    def __init__(self, val: int):
+        self.val: int = val
+        self.left: TreeNode | None = None
+        self.right: TreeNode | None = None
+
+
+testTreeNode = TreeNode(6)
+print(testTreeNode.val)
+
+# 初始化二叉树
+# 初始化节点
+n1 = TreeNode(val=1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n4 = TreeNode(4)
+n5 = TreeNode(5)
+# 构建节点之间的引用
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+
+# 插入与删除节点
+p = TreeNode(0)
+# 在 n1 -> n2 之间插入节点p
+n1.left = p
+p.left = n2
+# 删除节点p
+n1.left = n2
---
+++ binary_tree/ts/main.ts
@@ -0,0 +1,35 @@
+export class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val: number) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const testTreeNode = new TreeNode(6)
+console.log(testTreeNode.val)
+
+// 初始化二叉树
+// 初始化节点
+const n1 = new TreeNode(1)
+const n2 = new TreeNode(2)
+const n3 = new TreeNode(3)
+const n4 = new TreeNode(4)
+const n5 = new TreeNode(5)
+// 构建节点之间的引用
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+
+// 插入与删除节点
+const p = new TreeNode(0)
+// 在 n1 -> n2 之间插入节点 p
+n1.left = p
+p.left = n2
+// 删除节点 p
+n1.left = n2
+
