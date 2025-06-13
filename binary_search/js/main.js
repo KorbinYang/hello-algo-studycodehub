5bcaa41ff1b200ae193e513fb49e09fb2008dca3
Korbin <korbinyang@qq.com>
2025-06-13 18:36:01 +0800

    feat: 二分查找插入点索引

--- binary_search/js/main.js
+++ binary_search/js/main.js
@@ -52,3 +52,40 @@ const testTargetICRO = 6
 const testResultICRO = binary_search(testArrayICRO, testTargetICRO)
 console.log('二分查找（左闭右开区间）索引:', testResultICRO)
 
+// 二分查找插入点（无重复元素）
+function binary_search_insertion_simple(nums, target) {
+    let i = 0
+    let j = nums.length - 1
+
+    while (i <= j) {
+        let m = Math.floor((i + j) / 2)
+
+        if (target > nums[m]) {
+            i = m + 1
+        } else if (target < nums[m]) {
+            j = m - 1
+        } else {
+            return m
+        }
+    }
+
+    return i
+}
+
+const testInsertionArray = [1, 2, 3, 4, 5, 6, 7, 8, 9] // 有序数组
+const testInsertionTarget1 = 6
+const testInsertionTarget2 = 10
+const testResultInsertionIndex1 = binary_search_insertion_simple(
+    testInsertionArray,
+    testInsertionTarget1
+)
+const testResultInsertionIndex2 = binary_search_insertion_simple(
+    testInsertionArray,
+    testInsertionTarget2
+)
+console.log(
+    '二分查找插入点索引为:',
+    testResultInsertionIndex1,
+    testResultInsertionIndex2
+)
+
--- binary_search/python/main.py
+++ binary_search/python/main.py
@@ -40,3 +40,30 @@ testArrayICRO = [1, 4, 7, 9, 0, 3, 5, 6, 8, 3]
 testTargetICRO = 6
 testResultICRO = binary_search(testArrayICRO, testTargetICRO)
 print("二分查找（左闭右开区间）索引:", testResultICRO)
+
+
+def binary_search_insertion_simple(nums: list[int], target: int) -> int:
+    """二分查找插入点（无重复元素）"""
+    i, j = 0, len(nums) - 1  # 初始化双闭区间
+    while i <= j:
+        m = (i + j) // 2  # 计算中点索引 m
+        if target > nums[m]:
+            i = m + 1  # target 在区间[m+1, j]中
+        elif target < nums[m]:
+            j = m - 1  # target 在区间[i, m-1]中
+        else:
+            return m  # 找到 target ，返回插入点 m
+    # 未找到 target, 返回插入点 i
+    return i
+
+
+testInsertionArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # 有序数组
+testInsertionTarget1 = 6
+testInsertionTarget2 = 10
+testResultInsertionIndex1 = binary_search_insertion_simple(
+    testInsertionArray, testInsertionTarget1
+)
+testResultInsertionIndex2 = binary_search_insertion_simple(
+    testInsertionArray, testInsertionTarget2
+)
+print("二分查找插入点索引为:", testResultInsertionIndex1, testResultInsertionIndex2)
